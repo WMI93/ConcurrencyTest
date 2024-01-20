67b92dc201ced32f01256695f2fae7cec0ede557
wmi93 <wertanimohamedilyes@gmail.com>
2024-01-20 19:02:15 +0100

    Eg:CopyOnWriteArrayList  may avoid infinite loops.

--- src/Test/ConcurrencyTest.java
+++ src/Test/ConcurrencyTest.java
@@ -3,26 +3,15 @@ package Test;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConcurrencyTest {
-    //EG : comareing between AtomicInteger and Integer
-    static Integer integer =new Integer(0);
-    static AtomicInteger atomicInteger=new AtomicInteger(0);
-
-    static Runnable r1=()->System.out.println("Incremented value of atomic " +
-            "integer is : " + atomicInteger.incrementAndGet());
-
-    static Runnable r2=()->System.out.println("Incremented value of integer " +
-            "is : " + ++integer);
-
     public static void main(String[] args) {
-    //create three threads each for incrementing atomic and "normal" integers
-        for (int i = 0; i < 5; i++) {
-            new Thread(r1).start();
-            new Thread(r2).start();
+        //EG CopyOnWriteArrayList  may avoid infinite loops.
+        CopyOnWriteArrayList<Integer> iL =
+                new CopyOnWriteArrayList<>(new Integer[]{1, 2, 3});
+        for (Integer integer : iL) {
+            iL.add(4);
+        System.out.println(iL);
         }
-        CopyOnWriteArrayList
-
     }
 }
