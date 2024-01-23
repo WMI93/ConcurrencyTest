2f4ac13168aca3eb7f0e71409c669882334b0d86
wmi93 <wertanimohamedilyes@gmail.com>
2024-01-23 08:55:58 +0100

    Eg:using Lock interface

--- src/Test/ConcurrencyTest.java
+++ src/Test/ConcurrencyTest.java
@@ -5,13 +5,24 @@ import java.util.List;
 import java.util.concurrent.*;
 
 public class ConcurrencyTest {
+
     public static void main(String[] args) {
-        //EG CopyOnWriteArrayList  may avoid infinite loops.
-        CopyOnWriteArrayList<Integer> iL =
-                new CopyOnWriteArrayList<>(new Integer[]{1, 2, 3});
-        for (Integer integer : iL) {
-            iL.add(4);
-        System.out.println(iL);
+        Runnable r=() -> System.out.println("test Runable");
+        //Eg using Lock interface
+        Lock lock = new ReentrantLock();
+        // Acquire the lock
+        boolean tryLock=lock.tryLock();
+        boolean trylock_timeout=lock.tryLock(5,TimeUnit.SECONDS);
+        if(trylock_timeout) {
+            try {
+                // recommended to use lcock with try/catch block
+                lock.lock();
+                // Access the shared resource
+            } finally {
+                // Release the lock_è
+                lock.unlock();
+            }
         }
     }
 }
+
